1775bf7dee7140079054652cf3c88a82a5f354e5
Eduardo Amroginski Santos <eduardo-amroginski@hotmail.com>
2026-06-09 19:16:09 -0300

    Adicionando GET produtos

--- app.py
+++ app.py
@@ -25,5 +25,22 @@ def cadastrar():
     return jsonify(novo_item), 201
 
 
+@app.route("/api/produtos", methods=["GET"])
+def listar_produtos():
+    id_produto = request.args.get("id")
+    
+    if not LISTA_PRODUTOS:
+        return jsonify({"mensagem": "Nenhum produto cadastrado"}), 200
+    
+    if id_produto:
+        for produto in LISTA_PRODUTOS:
+            if int(produto.get("id")) == int(id_produto):
+                return jsonify(produto), 200
+                
+        return jsonify({"erro": "NOT_FOUND", "mensagem": "Produto não encontrado"}), 404
+
+    return jsonify(LISTA_PRODUTOS), 200
+
+
 if __name__ == "__main__":
     app.run(debug=True)
